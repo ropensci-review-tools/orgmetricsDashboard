a9f3d7dddf66b28b16e936bf62df848d0511ca8e
mpadge <mark.padgham@email.com>
2026-02-06 18:06:58 +0100

    export 'pull_orgmetrics_image' & add 'write_dotenv' fn

--- R/docker.R
+++ R/docker.R
@@ -69,13 +69,29 @@ docker_images_list <- function () {
     return (images)
 }
 
-docker_has_orgmetrics_image <- function () {
+docker_orgmetrics_image <- function () {
 
     di <- docker_images_list ()
-    any (grepl ("orgmetrics:latest", di$IMAGE))
+    grep ("orgmetrics:latest", di$IMAGE, value = TRUE)
 }
 
+#' Pull the 'orgmetrics' docker container for local use.
+#'
+#' @examples
+#' \dontrun{
+#' docker_pull_orgmetrics_image ()
+#' }
+#' @export
 docker_pull_orgmetrics_image <- function () {
+
+    if (length (docker_orgmetrics_image ()) > 0L) {
+        msg <- paste0 (
+            "Docker already has the 'orgmetrics' image; if you want ",
+            "to pull again, please 'docker rmi' this image first."
+        )
+        cli::cli_inform (msg)
+        return (FALSE)
+    }
 
     msg <- "Docker needs to 'pull' the orgmetrics container"
     cli::cli_inform (msg)
@@ -100,4 +116,72 @@ docker_pull_orgmetrics_image <- function () {
     system (paste0 ("docker pull ", orgmetrics_container_url))
 
     docker_has_orgmetrics_image ()
+}
+
+create_dotenv_file <- function (path = ".",
+                                repo_url = NULL,
+                                github_name = NULL,
+                                git_email = NULL,
+                                quarto_local = TRUE,
+                                quarto_publish = TRUE,
+                                quarto_provider = "gh-pages") {
+
+    if (is.null (repo_url)) {
+        repo_url <- gert::git_remote_list (repo = path)
+        repo_url <- grep ("github\\.com", repo_url$url, value = TRUE)
+    }
+    checkmate::assert_character (repo_url, len = 1L)
+
+    w <- whoami::whoami ()
+    if (is.null (github_name)) {
+        github_name <- w [["gh_username"]]
+    }
+    if (is.null (git_email)) {
+        git_email <- w [["email_address"]]
+    }
+    checkmate::assert_character (github_name, len = 1L)
+    checkmate::assert_character (git_email, len = 1L)
+
+    if (!quarto_publish && !quarto_local) {
+        cli::cli_abort (paste0 (
+            "Turning off publishing without creating a local ",
+            "quarto sub-directory means this function will do nothing. ",
+            "Stopping now"
+        ))
+    }
+
+    gh_tok <- gitcreds::gitcreds_get ()$password
+    checkmate::assert_character (gh_tok, len = 1L)
+
+    checkmate::assert_logical (quarto_local, len = 1L)
+    checkmate::assert_logical (quarto_publish, len = 1L)
+    quarto_local <- ifelse (quarto_local, "true", "false")
+    quarto_publish <- ifelse (quarto_publish, "true", "false")
+
+    checkmate::assert_character (quarto_provider, len = 1L)
+
+    env <- c (
+        "# ------ IMPORTANT! ------",
+        "# All variables should be defined here WITHOUT quotation marks.",
+        "",
+        "# GitHub credentials",
+        paste0 ("GITHUB_TOKEN=", gh_tok),
+        paste0 ("GITHUB_PAT=", gh_tok),
+        "",
+        "# Git user config",
+        paste0 ("GIT_USER_NAME=", github_name),
+        paste0 ("GIT_USER_EMAIL=", git_email),
+        "",
+        "# R-universe-like repo containing 'packages.json' file to populate dashboard.",
+        paste0 ("GIT_REMOTE_URL=", repo_url),
+        paste0 ("QUARTO_PUBLISH=", quarto_publish),
+        paste0 ("QUARTO_PROVIDER=", quarto_provider)
+    )
+
+    f <- fs::path_abs (".env")
+    writeLines (env, f)
+    cli::cli_alert_info ("Docker environment information written to {f}")
+    cli::cli_inform ("This file may be deleted after dashboard has been successfully deployed.")
+
+    return (f)
 }
